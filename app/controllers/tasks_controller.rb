2e8e52271172f8db16e8c5820b8f5df65e9b579e
sacha korsec <korsecsacha@gmail.com>
2018-11-21 11:24:04 +0100

    task manager

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,19 @@
+class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
